764f638a116620142746c0746261a1386e9c911b
Levysantiago <levyssantiago@gmail.com>
2020-12-09 20:33:20 -0300

    organizando codigo

--- helpers.py
+++ helpers.py
@@ -3,7 +3,41 @@ from PIL import Image, ImageOps
 def loadImageAsPixels(path):
     image = Image.open(path, 'r')
     pixels = image.getdata()
-    graypixels = (ImageOps.grayscale(image).getdata())
+    grayvalues = (ImageOps.grayscale(image).getdata())
 
-    # ImageOps.grayscale(image).show()
-    return pixels, graypixels, image, image.width, image.height
+    return pixels, grayvalues, image, image.width, image.height
+
+def selectPixels(grayvaluesA, grayvaluesB, width, height, limit):
+    selectedPixels = []
+    counter = 0
+    initialB = None
+    for i in range(width):
+        for j in range(height):
+            if(grayvaluesA[counter] < limit):
+                selectedPixels.append([(i,j), counter])
+            if(not initialB and grayvaluesB[counter] < limit):
+                initialB = [(i,j), counter]
+            counter += 1
+    initialA = selectedPixels[0]
+    return selectedPixels, initialA, initialB, 
+
+def selectPixelsBy(mapA, ipA, ipB, width):
+    mapB = [ipB]
+    for p in mapA:
+        if(p != ipA):
+            i = ipB[0][0] + abs(ipA[0][0] - p[0][0])
+            j = ipB[0][1] + abs(ipA[0][1] - p[0][1])
+            # Invertido, pois os pixels da imagem são organizados de maneira invertida
+            goTo = [(j,i), ipB[1] + abs(ipA[1] - p[1])]
+            if(i < width and j < width):
+                mapB.append(goTo)
+    return mapB
+
+def highlightPixels(A, B, mapA, mapB, gvA, gvB, limit, showImage):
+    colorIt = []
+    for i in range(len(mapB)):
+        if(abs(gvA[mapA[i][1]] - gvB[mapB[i][1]]) < limit):
+            colorIt.append(mapB[i])
+            B.putpixel(mapB[i][0], 0)
+    if(showImage):
+        B.show()
--- main.py
+++ main.py
@@ -1,54 +1,9 @@
 from helpers import *
-from calculator import *
 from PIL import ImageOps
 
-pixels, graypixels, image, width1, height1 = loadImageAsPixels('./assets/apple2.jpg')
-pixels2, graypixels2, image2, width2, height2 = loadImageAsPixels('./assets/apple3.jpg')
+pixelsA, gvA, A, width, height = loadImageAsPixels('./assets/apple2.jpg')
+pixelsB, gvB, B, widthB, heightB = loadImageAsPixels('./assets/apple3.jpg')
 
-totalPixels = width1*height1
-totalPixels2 = width2*height2
-
-initial = [(0,0), graypixels[0]]
-counter = 0
-selectedPixels = []
-initial2 = None
-
-# Por gray values
-for i in range(width1):
-    for j in range(height1):
-        if(graypixels[counter] < 200):
-            selectedPixels.append([(i,j), counter])
-        if(not initial2 and graypixels2[counter] < 200):
-            initial2 = [(i,j), counter]
-        counter += 1
-
-initial = selectedPixels[0]
-selectedPixels2 = [initial2]
-
-for p in selectedPixels:
-    if(p != initial):
-        # goTo = (initial2[0] + abs(initial[0] - p[0]), initial2[1] + abs(initial[1] - p[1]))
-        i = initial2[0][0] + abs(initial[0][0] - p[0][0])
-        j = initial2[0][1] + abs(initial[0][1] - p[0][1])
-        # Invertido, pois os pixels da imagem são organizados de maneira invertida
-        goTo = [(j,i), initial2[1] + abs(initial[1] - p[1])]
-        if(i < 200 and j < 200):
-            selectedPixels2.append(goTo)
-
-# print(initial)
-# print(initial2)
-# print(selectedPixels[1])
-# print(selectedPixels2[1])
-
-colorIt = []
-
-for i in range(len(selectedPixels2)):
-    if(abs(graypixels[selectedPixels[i][1]] - graypixels2[selectedPixels2[i][1]]) < 250 ):
-        colorIt.append(selectedPixels2[i])
-        # print(selectedPixels2[i][0])
-        image2.putpixel(selectedPixels2[i][0], 0)
-
-image2.show()
-
-
-# print(colorIt)
+mapA, ipA, ipB = selectPixels(gvA, gvB, width, height, 200)
+mapB = selectPixelsBy(mapA, ipA, ipB, width)
+highlightPixels(A, B, mapA, mapB, gvA, gvB, 250, True)
