095d487fe3f9f0ef7d886fb89ee14bed468225b8
Levysantiago <levyssantiago@gmail.com>
2020-12-09 21:25:15 -0300

    atualizando nomes de imagens

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from lr2Matching import *
 
 # Abrindo imagens
-A, width, height = loadImage('./assets/apple2.jpg')
-B, widthB, heightB = loadImage('./assets/apple3.jpg')
+A, width, height = loadImage('./assets/apple.jpg')
+B, widthB, heightB = loadImage('./assets/apple2.jpg')
 
 # Executando algoritmo
 lr2Matching(A, B)
